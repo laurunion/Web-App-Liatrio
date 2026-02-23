8d60184038e89811c5e058e8b9351a77c97fe191
laurunion <166334905+laurunion@users.noreply.github.com>
2026-02-23 05:26:30 +0000

    complete single endpoint fiber app

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
-import "github.com/gofiber/fiber/v3"
+import (
+    "time"
+    "github.com/gofiber/fiber/v3"
+)
+
+type Response struct{
+    Message    string  `json:"message"`
+    Timestamp  int64   `json:"timestamp"`  
+}
 
 func main() {
     app := fiber.New()
 
     app.Get("/", func(c fiber.Ctx) error {
-        return c.SendString("Hello, World!")
+        response := Response{
+            Message: "My name is Lauren",
+            Timestamp: time.Now().Unix(),
+        }
+        return c.JSON(response)
     })
 
     app.Listen(":3000")
